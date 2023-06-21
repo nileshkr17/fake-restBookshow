d5a2e5a4c3462bbe68ef5a0fd7282acf066206cf
Sonali12b <sonali24bhatta@gmail.com>
2023-06-21 17:06:43 +0530

    [button-added]

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -7,7 +7,10 @@ function App() {
     <div className="App">
       <Nav/>
       <Landing/>
-    </div>
+      <button className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 p-4 ">
+  Save changes
+</button>
+</div>
   );
 }
 
