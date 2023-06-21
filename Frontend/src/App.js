18dd4c9d1afe01bf9d3406c81f3bdfdad777665b
Nilesh <ece.20becd76@silicon.ac.in>
2023-06-21 16:56:08 +0530

    []

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Landing from './pages/Landing';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav/>
+      <Landing/>
     </div>
   );
 }
