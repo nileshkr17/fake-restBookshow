b2e03a400907c60dfef0d800726d260629934d16
Sahil1525 <sahilsaha15255@gmail.com>
2023-06-22 10:22:59 +0530

    Made Navbar

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
 import Landing from './pages/Landing';
-import Nav from './components/Nav';
+import Navbar from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
-      <Landing/>
-      <button className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 p-4 ">
-  Save changes
-</button>
+      <Navbar/>
 </div>
   );
 }
--- Frontend/src/components/Nav.jsx
+++ Frontend/src/components/Nav.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './nav.css';
 
-const Nav = () => {
+const Navbar = () => {
   return (
-    <nav className=''>
-    <ul>
-      <li>Home</li>
-      <li>here</li>
-    </ul>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <a href="/">BookMyShow</a>
+      </div>
+      <div className="navbar-links">
+        <a href="/movies">Movies</a><br></br>
+        <a href="/events">Events</a><br></br>
+        <a href="/sports">Sports</a><br></br>
+        <a href="/offers">Offers</a><br></br>
+      </div>
+      <div className="navbar-account">
+        <a href="/login">Log In</a><br></br>
+        <a href="/signup">Sign Up</a>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Navbar;
