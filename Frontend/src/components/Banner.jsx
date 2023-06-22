d4e4f417d61722f10a07869adf1e7e35a2730332
Nilesh <ece.20becd76@silicon.ac.in>
2023-06-22 12:51:01 +0530

    [fixed tailwindcss-bugs]

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
+import "../index.css";
 
 const Banner = () => {
   return (
-   
-   <>banner</> // <div className='bg-blue-900 py-20'> Banner</div>
-  )
-}
+    <div className="bg-black w-full h-screen py-25 text-white ">
+      <div className="max-w-[1240px] mx-auto ">
+        <h1 className="text-6xl text-center  text-white">Hello World</h1>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Button
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
