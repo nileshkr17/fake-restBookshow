cf25f0a107f8aa58e2fd5e9db834ad10552f578d
Sonali12b <sonali24bhatta@gmail.com>
2023-06-23 10:30:30 +0530

    [get-done-in-latest]

--- Frontend/src/components/Latest.jsx
+++ Frontend/src/components/Latest.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState,useEffect } from 'react';
 
 const Latest = () => {
   const movieData = [
@@ -33,6 +34,22 @@ const Latest = () => {
       },
    
   ];
+  const [arr, setArr] = useState([]);
+  const [newarr, setnewArr] = useState([]);
+   /*getdata*/
+   const getData = async () => {
+    await fetch("http://localhost:3001/movies", {
+      method: "GET",
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setArr(data);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
 
   const handleMovieClick = (id) => {
     // Perform navigation logic with the provided movie id
@@ -44,7 +61,7 @@ const Latest = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <h2 className="sm:text-3xl md:text-4xl lg:text-5xl font-bold pb-10 text-white">Latest Movies</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {movieData.map((movie) => (
+          {arr.map((movie) => { return (
             <div
               key={movie.id}
               className="relative group overflow-hidden rounded-md cursor-pointer"
@@ -53,7 +70,7 @@ const Latest = () => {
               <img
                 src={movie.imageUrl}
                 alt="Latest movie"
-                className="object-fit w-full h-60 sm:h-80 md:h-30 lg:h-30 transition-opacity duration-300"
+                className="object-fit w-80 h-60 sm:h-80 md:h-30 lg:h-30 transition-opacity duration-300"
               />
               <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-md opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col items-center justify-center">
                 <p className="text-white text-lg font-bold">{movie.title}</p>
@@ -61,7 +78,7 @@ const Latest = () => {
                   <span className="text-yellow-400 mr-1">&#9733;</span>
                   <span className="text-white">{movie.rating}</span>
                 </div>
-                <p className="text-white mt-2 px-4 text-center">{movie.description}</p>
+                <p className="text-white mt-2 px-4 text-center">{movie.details}</p>
                 <button
                   className="mt-2 px-4 py-2 bg-red-500 text-white rounded-md"
                   onClick={() => handleMovieClick(movie.id)}
@@ -70,7 +87,7 @@ const Latest = () => {
                 </button>
               </div>
             </div>
-          ))}
+          )})}
         </div>
       </div>
     </div>
