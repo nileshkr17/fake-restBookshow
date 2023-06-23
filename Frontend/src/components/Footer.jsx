6b1de885e6fb4a8822591b791182c791eff8c536
Sonali12b <sonali24bhatta@gmail.com>
2023-06-23 21:32:55 +0530

    [link-added]

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -38,10 +38,17 @@
 import React from 'react';
 
 const Footer = () => {
+  const repoLink = 'https://github.com/nileshkr17/fake-restBookshow.git';
+
   return (
     <footer className="bg-black py-4 shadow-lg border-t-4 border-slate-700">
       <div className="container mx-auto text-white text-center">
-        <p className="text-white">Made with ❤️ by <span className='italic'>.gitignore</span></p>
+        <p className="text-white">
+          Made with ❤️ by <span className='italic'><a href={repoLink} className="text-white underline">
+          .gitignore
+          </a></span>
+          <br />
+        </p>
       </div>
     </footer>
   );
