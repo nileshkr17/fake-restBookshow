9c376610a7e88446358b3ac82d83bc794e444efb
Nilesh <ece.20becd76@silicon.ac.in>
2023-06-24 01:08:20 +0530

    [last-modification@movie]

--- Frontend/src/components/Loader.jsx
+++ Frontend/src/components/Loader.jsx
@@ -4,6 +4,8 @@ const Loader = () => {
   return (
     <div className="loader-container h-screen bg-black">
       <div className="loader"></div>
+      {/* a loader msg */}
+        <h2 className="text-white text-2xl font-bold">Loading...</h2>
     </div>
   );
 };
